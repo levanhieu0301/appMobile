b08f2dec5359f5adf1fbae9b4e2a93ca48a7ea0e
lvh0301 <levanhieu0301@gmail.com>
2026-04-12 09:30:07 +0700

    Chỉnh sửa toolbar

--- app/src/main/java/com/example/appcar/AdminActivity.kt
+++ app/src/main/java/com/example/appcar/AdminActivity.kt
@@ -46,9 +46,6 @@ class AdminActivity : AppCompatActivity() {
         findViewById<TextView>(R.id.menuVoucher).setOnClickListener {
             startActivity(Intent(this, ManagePromotionActivity::class.java))
         }
-        findViewById<TextView>(R.id.menuVoucher).setOnClickListener {
-            startActivity(Intent(this, ManagePromotionActivity::class.java))
-        }
 
         findViewById<TextView>(R.id.menuHistory).setOnClickListener {
             startActivity(Intent(this, HistoryActivity::class.java))
