42e2d7794aeb5823d20e499910909f8fdc7ae0bc
lvh0301 <levanhieu0301@gmail.com>
2026-04-12 16:57:18 +0700

    commit tạm

--- app/src/main/java/com/example/appcar/AdminActivity.kt
+++ app/src/main/java/com/example/appcar/AdminActivity.kt
@@ -50,6 +50,7 @@ class AdminActivity : AppCompatActivity() {
         findViewById<TextView>(R.id.menuHistory).setOnClickListener {
             startActivity(Intent(this, HistoryActivity::class.java))
         }
+
         findViewById<TextView>(R.id.menuLogout).setOnClickListener {
 
             //Xóa session nếu có (optional)
